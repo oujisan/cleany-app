0055a46ead39d7de5f84ebcd8b41ab6c963d63a2
oujisan <oujisan.main@gmail.com>
2025-05-01 14:37:22 +0700

    Menambahkan Template Empty Flutter

---
+++ cleany_app/README.md
@@ -0,0 +1,3 @@
+# cleany_app
+
+A new Flutter project.
---
+++ cleany_app/android/app/src/main/kotlin/com/example/cleany_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cleany_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
