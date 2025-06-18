0250b1561ea852719c4dac3eef53a5507f7ce758
takioden <takiiii4876@gmail.com>
2025-06-19 02:53:34 +0700

    profile services

---
+++ cleany_app/lib/src/services/profile_service.dart
@@ -0,0 +1,95 @@
+import 'package:cleany_app/src/models/profile_model.dart';
+import 'package:cleany_app/core/constant.dart';
+import 'package:http/http.dart' as http;
+import 'package:cleany_app/core/secure_storage.dart';
+import 'dart:convert';
+
+class ProfileService {
+    String _message = '';
+    String _error = '';
+  
+    String get getMessage => _message;
+    String get getError => _error;
+ Future<ProfileModel> fetchUserProfile(String userId) async {
+  final url = Uri.parse(AppConstants.apiFetchUserProfileUrl(userId));
+  try {
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ${await SecureStorage.read(AppConstants.keyToken)}',
+      },
+    );
+  
+    final apiResponse = json.decode(response.body);
+
+    if (response.statusCode == 200 && apiResponse['data'] != null) {
+      return ProfileModel.fromJson(apiResponse['data']);
+    } else {
+      _message = apiResponse['message'];
+      _error = apiResponse['error'];
+      return ProfileModel.empty(); 
+    }
+  } catch (e) {
+    _message = 'Exception occurred';
+    _error = e.toString();
+    return ProfileModel.empty();
+  }
+}
+
+  Future<ProfileModel> updateUserProfile(ProfileModel profile) async {
+    final url = Uri.parse(AppConstants.updateUserProfileUrl(profile.username));
+    try {
+      final response = await http.put(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': 'Bearer ${await SecureStorage.read(AppConstants.keyToken)}',
+        },
+        body: json.encode(profile.toJson()),
+      );
+  
+      final apiResponse = json.decode(response.body);
+  
+      if (response.statusCode == 200 && apiResponse['data'] != null) {
+        return ProfileModel.fromJson(apiResponse['data']);
+      } else {
+        _message = apiResponse['message'];
+        _error = apiResponse['error'];
+        return ProfileModel.empty(); 
+      }
+    } catch (e) {
+      _message = 'Exception occurred';
+      _error = e.toString();
+      return ProfileModel.empty();
+    }
+  }
+
+  Future<bool> deleteUserProfile(String userId) async {
+    final url = Uri.parse(AppConstants.apiDeleteUserProfileUrl(userId));
+    try {
+      final response = await http.delete(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': 'Bearer ${await SecureStorage.read(AppConstants.keyToken)}',
+        },
+      );
+
+      final apiResponse = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        _message = apiResponse['message'];
+        _error = apiResponse['error'];
+        return false;
+      }
+    } catch (e) {
+      _message = 'Exception occurred';
+      _error = e.toString();
+      return false;
+    }
+  }
+}
+
